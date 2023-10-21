e5bc67bd34f9f50c0bd2046d60786515f7b6017e
Pauiiiii <paupauabrenica16@gmail.com>
2023-10-21 17:23:46 +0800

    navbar and searchbar ui

---
+++ components/SearchBar.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton} from '@mui/material';
+import { Search } from '@mui/icons-material';
+import { red } from "@mui/material/colors";
+
+
+function SearchBar() {
+  return (
+    <Paper component="form"
+    onSubmit={() => {}}
+    sx={{
+      borderRaduis: 20, border: '1px solid #e3e3e3', pl: 2, mr: { sm: 5 }
+    }}>
+      <input className="search-bar"
+      placeholder="Search..."
+      value=""
+      onChange={() => {}}/>
+      <IconButton type="submit" sx={{ p: '10px', color: 'red'}}>
+        <Search />
+      </IconButton>
+    </Paper>
+  )
+};
+
+export default SearchBar
