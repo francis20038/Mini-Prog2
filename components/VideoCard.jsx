df374460ebfc7c7d00e6b0b1ec7281932cd106c1
Pauiiiii <paupauabrenica16@gmail.com>
2023-11-11 10:35:33 +0800

    progress2

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from './utils/constant'
 
-function VideoCard() {
+function VideoCard({ video: { id: { videoId }, snippet}}) {
   return (
-    <div>VideoCard</div>
+    <Card sx={{ width: { md: '320px', sx: '100%' }, boxShadow: 'none', borderRadius: 'none' }}>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+      <CardMedia 
+      image={snippet?.thumbnail?.high?.url}
+      alt={snippet?.title}
+      sx={{ width: 350, height: 180 }} />
+      </Link>
+      <CardContent sx={{ background: '#1e1e1e', height: '50px' }} >
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+          {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0,60)}
+        </Typography>
+        </Link>
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+        <Typography variant="subtitle2" fontWeight="bold" color="gray">
+          {snippet?.channelTitle || demoChannelTitle}
+          <CheckCircle sx={{ fontSize: 12, color: "gray", ml: '5px'}}/>
+        </Typography>
+        </Link>
+      </CardContent>
+    </Card>
   )
 }
 
--- components/utils/fetchFromApi.jsx
+++ components/utils/fetchFromApi.jsx
@@ -9,7 +9,7 @@ const options = {
   headers: {
     'X-RapidAPI-Key': 'f0b07d804amsh4a9a85c43929268p1d0fc9jsna01092c6fdab',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-  },
+  }
 };
 
 export const fetchFromAPI = async (url) => {
